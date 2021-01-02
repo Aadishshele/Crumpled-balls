cddfd1868b766aed03546e3725c48b0e7143031f
Aadishshele <74540619+Aadishshele@users.noreply.github.com>
2021-01-02 11:02:36 +0800

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,24 @@
+class Ball {
+    constructor(x,y,r){
+        var options={
+            restitution : 0,
+            friction : 0.5,
+            density : 1.2,
+            isStatic : false
+        }
+        this.x = x
+        this.y = y
+        this.r = r
+        
+        this.body = Bodies.circle(this.x,this.y,this.r/2,options)
+        World.add(world,this.body)
+    }
+    display(){
+        push()
+        translate(this.body.position.x,this.body.position.y)
+        //stroke("blue")
+        ellipse(0,0,this.r,)
+        pop()
+            
+    }
+}
